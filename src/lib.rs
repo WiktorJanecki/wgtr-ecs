03c424a5eb64cdec97f00490a8c048f258e473e8
Wiktor Janecki <j.wiktor05@interia.pl>
2022-06-17 12:52:21 +0200

    Add registering components and adding entities

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod wgtr{
-    use std::{any::{Any, TypeId}, collections::HashMap};
+    use std::{any::{Any, TypeId}, collections::HashMap, cell::RefCell, rc::Rc};
 
     #[derive(Default)]
     pub struct World{
         resources: HashMap<TypeId, Box<dyn Any>>,
+        components: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any + 'static>>>>>,
     }
 
 
@@ -11,6 +12,34 @@ pub mod wgtr{
         pub fn new() -> Self{
             Self::default()
         }
+
+        pub fn register_component<T: Any>(&mut self){
+            let type_id = TypeId::of::<T>();
+            self.components.insert(type_id, vec![]);
+        }
+
+        pub fn create_entity(&mut self) -> &mut Self{
+            self.components.iter_mut().for_each(|(_key, components)| components.push(None));
+            self
+        }
+
+        pub fn with_component(&mut self, data: impl Any) -> Result<&mut Self, &'static str>{
+            let type_id = data.type_id();
+
+            if let Some(components) = self.components.get_mut(&type_id){
+                let last_component = components.last_mut()
+                    .ok_or_else(|| "Could not find the last component")
+                    .unwrap();
+                    *last_component = Some(Rc::new(RefCell::new(data)));
+            }
+            else{
+                return Err("Tried to use with_comopnent with a component that hasnt been registered");
+            }
+            Ok(self)
+        }
+
+
+
         pub fn add_resource(&mut self, resource_data: impl Any){
             let type_id = resource_data.type_id();
             self.resources.insert(type_id, Box::new(resource_data));
---
+++ tests/entities.rs
@@ -0,0 +1,16 @@
+use wgtr_ecs::wgtr::*;
+
+#[test]
+fn create_entity() -> Result<(),&'static str>{
+    let mut world = World::new();
+    world.register_component::<Health>();
+    world.register_component::<Speed>();
+
+    world.create_entity()
+        .with_component(Health(100))?
+        .with_component(Speed(10))?;
+    Ok(())
+}
+
+struct Health(pub u32);
+struct Speed(pub u32);
